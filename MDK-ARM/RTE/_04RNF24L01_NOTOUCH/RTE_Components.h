1918559314b4040bb5c78f6ebfd73918f2b7b4cb
HuangYi <123337671@qq.com>
2021-05-10 14:45:13 +0800

    add : 支持MDK-ARM

---
+++ MDK-ARM/RTE/_04RNF24L01_NOTOUCH/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '04RNF24L01_NOTOUCH' 
+ * Target:  '04RNF24L01_NOTOUCH' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
